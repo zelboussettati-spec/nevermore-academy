68bedd74c80bcaaeb8fe45d77e281f791ff2d620
Faysal_Aden <s1217097@student.windesheim.nl>
2025-05-18 17:56:13 +0200

    2 extra data sets toegevoegd

--- startcode_tsp_basis - Copy.py
+++ startcode_tsp_basis - Copy.py
@@ -5,13 +5,25 @@ start = 'Keuken'
 cities = ['Tafel1', 'Tafel2', 'Tafel3']
 
 # Definieer de afstandsmatrix als een dictionary van dictionaries.
-distance = {
+distance_3 = {
     'Keuken': {'Keuken': 0,   'Tafel1': 80,  'Tafel2': 40,  'Tafel3': 70},
     'Tafel1': {'Keuken': 80,  'Tafel1': 0,   'Tafel2': 50,  'Tafel3': 35},
     'Tafel2': {'Keuken': 40,  'Tafel1': 50,  'Tafel2': 0,   'Tafel3': 55},
     'Tafel3': {'Keuken': 70,  'Tafel1': 35,  'Tafel2': 55,  'Tafel3': 0}
 }
+distance_4 = {
+    'Keuken': {'Keuken': 0,   'Tafel1': 60,  'Tafel2': 45,  'Tafel3': 70,  'Tafel4': 55},
+    'Tafel1': {'Keuken': 60,  'Tafel1': 0,   'Tafel2': 35,  'Tafel3': 40,  'Tafel4': 30},
+    'Tafel2': {'Keuken': 45,  'Tafel1': 35,  'Tafel2': 0,   'Tafel3': 50,  'Tafel4': 20},
+    'Tafel3': {'Keuken': 55,  'Tafel1': 30,  'Tafel2': 20,  'Tafel3': 25,  'Tafel4': 0}
+}
 
+distance_6 = {
+    'Keuken': {'Keuken': 0,   'Tafel1': 65,  'Tafel2': 50,  'Tafel3': 70,  'Tafel4': 60,  'Tafel5': 55,  'Tafel6': 45},
+    'Tafel1': {'Keuken': 65,  'Tafel1': 0,   'Tafel2': 25,  'Tafel3': 40,  'Tafel4': 35,  'Tafel5': 45,  'Tafel6': 50},
+    'Tafel2': {'Keuken': 70,  'Tafel1': 40,  'Tafel2': 30,  'Tafel3': 0,   'Tafel4': 25,  'Tafel5': 30,  'Tafel6': 20},
+    'Tafel3': {'Keuken': 45,  'Tafel1': 50,  'Tafel2': 35,  'Tafel3': 20,  'Tafel4': 25,  'Tafel5': 10,  'Tafel6': 0}
+}
 
 
 # Begin met een grote afstand, zodat elke route hiermee vergeleken kan worden.
